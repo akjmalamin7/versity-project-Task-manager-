13eb4e03493c32527769f7eb537aa0ddeb87cb22
akjmalamin7 <thpi205526@gmail.com>
2025-02-07 23:48:20 +0600

    Refactor CanceledIcon component for improved SVG structure and styling; update stroke color for better visibility

--- src/assets/icons/canceledIcon/CanceledIcon.tsx
+++ src/assets/icons/canceledIcon/CanceledIcon.tsx
@@ -2,7 +2,7 @@
 const CanceledIcon = () => {
   return (
 <svg width="21" height="21" viewBox="0 0 21 21" fill="#9ca3af" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd" clipRule="evenodd" d="M4.5 2.5H16.5C17.6046 2.5 18.5 3.39543 18.5 4.5V16.5C18.5 17.6046 17.6046 18.5 16.5 18.5H4.5C3.39543 18.5 2.5 17.6046 2.5 16.5V4.5C2.5 3.39543 3.39543 2.5 4.5 2.5Z" fill="#9ca3af" stroke="#9ca3af" strokeLinecap="round" strokeLinejoin="round"/>
+<path fillRule="evenodd" clipRule="evenodd" d="M4.5 2.5H16.5C17.6046 2.5 18.5 3.39543 18.5 4.5V16.5C18.5 17.6046 17.6046 18.5 16.5 18.5H4.5C3.39543 18.5 2.5 17.6046 2.5 16.5V4.5C2.5 3.39543 3.39543 2.5 4.5 2.5Z" fill="#9ca3af" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
 <path d="M2.5 6.5H18.5" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
 <path d="M13.5 12.5H7.5" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
